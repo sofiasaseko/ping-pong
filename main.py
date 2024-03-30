a3e0319b3e6ee56e631bce6d86ab2fa8c68798b5
sofiasaseko <sofiasaseko@gmail.com>
2024-03-30 14:27:49 +0300

    lalalal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygame import*
 
 SCREENSIZE=(700,700)
-BACKCOLOR=(255,227,227)
+BACKCOLOR=(176,196,222)
 
 window=display.set_mode(SCREENSIZE)
 display.set_caption('ping-pong')
@@ -11,10 +11,10 @@ timer=time.Clock()
 run=True
 
 class GameSprite(sprite.Sprite):
-    def __init__ (self,img,x,y,width,height):
+    def __init__ (self,img,x,y,speed,width,height):
         super().__init__()
-        self.image=transform.scale(image.load(img),wids,height)
-        self.speed=0
+        self.image=transform.scale(image.load(img),(width,height))
+        self.speed=speed
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
@@ -26,22 +26,28 @@ class Player(GameSprite):
     def update_left(self):
         keys=key.get_pressed()
         if keys[K_w] and self.rect.y>0:
-            self.rect.y -=self.speede
+            self.rect.y -=self.speed
         if keys[K_s] and self.rect.y +  self.rect.height< SCREENSIZE[1]:
             self.rect.y +=self.speed
 
     def update_right(self):
         keys=key.get_pressed()
-        if keys[K_Up] and self.rect.y>0:
-            self.rect.y -=self.speede
+        if keys[K_UP] and self.rect.y>0:
+            self.rect.y -=self.speed
         if keys[K_DOWN] and self.rect.y +  self.rect.height< SCREENSIZE[1]:
             self.rect.y +=self.speed
+
+platform_left = Player("платформа.jpeg",20,320,4,25,99)
+platform_right = Player("платформа.jpeg",660,320,4,25,99)
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run=False
-    
     window.fill(BACKCOLOR)
+    platform_left.reset()
+    platform_right.reset()
+    platform_left.update_left()
+    platform_right.update_right()
     display.update()
     timer.tick(60)
